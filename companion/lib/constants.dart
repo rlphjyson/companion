1e2ff1709be09a98411e10e604d1293c25347292
Ralph Jason Burgos <91693827+rlphjyson@users.noreply.github.com>
2024-08-12 13:10:44 +0800

    Update constants.dart

--- companion/lib/constants.dart
+++ companion/lib/constants.dart
@@ -1,3 +1,1 @@
-const String GEMINI_API_KEY = "AIzaSyAfUTgzg2Sq7GvfHqWbsrCkliQ0gsDNrOE";
-const String TTS_INPUT =
-    "Hello World, my name is Ralph Burgos. I am a software engineer.";
+const String GEMINI_API_KEY = "YOUR GEMINI API KEY";
